5defbf97ba9f809ef781836bcaf1c6d47dc51a51
Jay-kod <james1ofourtime@gmail.com>
2024-09-20 17:04:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+// Function to open the form
+function openForm() {
+    document.getElementById("contactForm").style.display = "block";
+    document.getElementById("formBackground").style.display = "block";
+}
+
+// Function to close the form
+function closeForm() {
+    document.getElementById("contactForm").style.display = "none";
+    document.getElementById("formBackground").style.display = "none";
+}
